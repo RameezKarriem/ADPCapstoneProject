fc9695cf3362fb1c8fef2d3dc793a08200754283
Qaasim <qaasimisaacs2003@gmail.com>
2026-03-27 16:55:34 +0200

    I have completed the InstructorRepository and the InstructorRepositoryTest. Review my work

---
+++ src/main/java/za/ac/cput/repository/InstructorRepository.java
@@ -0,0 +1,82 @@
+/* InstructorRepository.java
+InstructorRepository model class
+Author: Qaasim Isaacs (222544422)
+Date: 27 March 2026
+*/
+package za.ac.cput.repository;
+
+import za.ac.cput.domain.Instructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InstructorRepository implements IRepository<Instructor, String> {
+
+    // Singleton instance
+    private static InstructorRepository repository = null;
+
+    // In memory list
+    private List<Instructor> instructorList;
+
+    // Private constructor
+    private InstructorRepository() {
+        instructorList = new ArrayList<>();
+    }
+
+    // Get singleton instance
+    public static InstructorRepository getRepository() {
+        if (repository == null) {
+            repository = new InstructorRepository();
+        }
+        return repository;
+    }
+
+    @Override
+    public Instructor create(Instructor instructor) {
+        boolean success = instructorList.add(instructor);
+
+        if (success) {
+            return instructor;
+        }
+        return null;
+    }
+
+    @Override
+    public Instructor read(String instructorId) {
+        for (Instructor instructor : instructorList) {
+            if (instructor.getInstructorId().equals(instructorId)) {
+                return instructor;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Instructor update(Instructor instructor) {
+        String id = instructor.getInstructorId();
+        Instructor oldInstructor = read(id);
+
+        if (oldInstructor != null) {
+            instructorList.remove(oldInstructor);
+            instructorList.add(instructor);
+            return instructor;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        Instructor instructorToDelete = read(id);
+
+        if (instructorToDelete != null) {
+            instructorList.remove(instructorToDelete);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<Instructor> getAll() {
+        return instructorList;
+    }
+}
